0b660d0b31a41fcc7ecebf1e138c014f16732d6d
Christopher McLain <chris@humansarehuman.com>
2019-08-07 20:57:40 -0400

    linux files were all updated
    working

--- b2factor/b2f.py
+++ b2factor/b2f.py
@@ -1,55 +1,160 @@
 #!/usr/bin/env python
 
+'''
+    Created on Feb 28, 2019
+    @author: cjm10
+    '''
+
 import sys
+import os
 import json
 import urllib
+try:
+    from urllib.parse import unquote
+except ImportError:
+    from urlparse import unquote
 import urllib2
+from __builtin__ import False
 
-
-SERVER_ID="YOUR_SERVER_ID"
 endpoint = "https://secure.blue2factor.com/b2f-prox"
+setupEndpoint = "https://secure.blue2factor.com/b2f-server-setup"
+configFile = "b2f.config"
 FAILURE = 1
 SUCCESS = 0
-def main(args=None):
+TESTING = False
+
+def main():
     exitCode = FAILURE
     args = sys.argv
-    if SERVER_ID == "YOUR_SERVER_ID":
-        sys.stdout.write("\n\n")
-        sys.stdout.write("Please change the server ID in /etc/b2factor/b2f.sh \n")
-        sys.stdout.write("to enable Blue2Factor security\n\n")
-        exitCode = SUCCESS
-    else:    
-        if args is not None and len(args) > 2:
-            if getProximityFromServer(args[1], args[2]):
-                exitCode = SUCCESS
+    if args is not None and len(args) > 2:
+        if getProximityFromServer(args[1], args[2]):
+            exitCode = SUCCESS
+    else:
+        if len(args) == 2:
+            prefix = args[1][:4]
+            uuid = getServerUuidFromArg(args[1])
+            print("uuid: " + uuid + ", pref: " + prefix)
+            if prefix == "inst":
+                if setup(uuid):
+                    exitCode = SUCCESS
+            elif prefix == "vali":
+                if confirm(uuid):
+                    exitCode = SUCCESS
+            elif prefix == "dele":
+                if delete(uuid):
+                    exitCode = SUCCESS
         else:
-            sys.stdout.write("Blue2Factor authentication failed.\n\n")
+            sys.stdout.write("not enough arguments: " + str(len(args)) + "\n\n")
             sys.stdout.write("Make sure that the proper code has been added to ssh_config\n")
             sys.stdout.write("on your machine and to sshd_config on the server you're\n")
             sys.stdout.write("connecting to\n\n")
             sys.stdout.write("For assistence, contact your network administrator or visit\n")
             sys.stdout.write("www.blue2factor.com/ssh_setup\n")
+            #just in case we need to override a failure
             if getProximityFromServer("", ""):
                 exitCode = SUCCESS
-        sys.exit(exitCode)
+    sys.exit(exitCode)
+
+def getServerUuidFromArg(arg):
+    return arg[4:]
+
+def writePublicKeyToFile(publicKey):
+    keyFile = getBaseDir() + "/b2factor/keys/b2f.pem"
+    f = open(keyFile, "w+")
+    f.write("-----BEGIN PUBLIC KEY-----\n")
+    f.write(publicKey)
+    f.write("\n-----END PUBLIC KEY-----")
+    f.close()
+
+def delete(serverUuid):
+    success, _ = getDeleteUrl(setupEndpoint, serverUuid)
+    return success
+
+def confirm(serverUuid):
+    success, _ = getConfirmUrl(setupEndpoint, serverUuid)
+    return success
+
+def setup(serverUuid):
+    success = False
+    if (len(serverUuid)==36):
+        success, pubKey = getSetupUrl(setupEndpoint, serverUuid)
+        if success:
+            writePublicKeyToFile(pubKey)
+        else:
+            sys.stdout.write("We could not verify that you entered the code into the Blue2Factor Admin Console.")
+            if (pubKey is not None and pubKey != ""):
+                sys.stdout.write("Error: " + pubKey)
+    else:
+        sys.stdout.write("Installation was unsuccessful - bad argument")
+    return success
 
 def getProximityFromServer(token, devId):
     success = False
-    success, reason = getUrlOutcome(endpoint, token, devId)
-    if not success:
-        if reason != None and reason != "":
+    if len(token) > 19:
+        success, reason = getUrlOutcome(endpoint, token, devId)
+        if not success:
             sys.stdout.write("Blue2Factor failed with reason: \"" + reason + "\"\n")
-        else:
-            sys.stdout.write("Blue2Factor authentication failed.");
-    elif reason != "":
-        sys.stdout.write(str(reason) + "\n")
+        elif reason != "":
+            sys.stdout.write(str(reason) + "\n")
+    else:
+        sys.stdout.write("invalid arg: " + token + "\n")
     return success
+
+def getConfigVal(keyText):
+    value = ""
+    __location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    f = open(os.path.join(__location__, 'b2f.config'));
+#    filePath = os.path.join(sys.path[0], 'b2f.config'
+#    sys.stdout.write("opening " + str(os.path.join(__location__, 'b2f.config')) + "\n")
+#    f = open(filePath, "r")
+    for line in f:
+        if "=" in line:
+            lineSplit = line.split('=')
+            if lineSplit[0] == keyText:
+                value = lineSplit[1].rstrip()
+#                sys.stdout.write("found: " + value)
+                break
+    f.close()
+    return value
+
+def getBaseDir():
+    if TESTING:
+        return "/Users/cjm10"
+    return getConfigVal('BASE_INSTALL_DIR')
+
+def getServerId():
+    return getConfigVal('SERVER_ID')
+
+def getDeleteUrl(url, serverId):
+    params = {'uuid' : serverId, 'purp' : 'dele'}
+    success, reason = fetchUrl(url, params)
+    return success, reason
+
+def getConfirmUrl(url, serverId):
+    params = {'uuid' : serverId, 'purp' : 'vali'}
+    success, reason = fetchUrl(url, params)
+    return success, reason
+
+def getSetupUrl(url, serverId):
+    params = {'uuid' : serverId, 'purp' : 'inst'}
+    success, reason = fetchUrl(url, params)
+    return success, reason
 
 def getUrlOutcome(url, token, devId):
     success = False
+    serverId = getServerId();
+    if serverId != None and serverId != "YOUR_SERVER_ID" and serverId != "":
+        params = {'tok' : token, 'coId' : serverId, 'b2fbe' : 'svr', 'dId' : devId}
+        success, reason = fetchUrl(url, params)
+    else:
+        reason = ("Configuration file error: check " + os.path.dirname(os.path.abspath(__file__)) + "/b2f.config " +
+                  "to make sure it's set up correctly")
+    return success, reason
+
+def fetchUrl(url, params):
+    success = False
     reason = ""
-    #sys.stdout.write("retrieving: " + url + "\n")
-    params = {'tok' : token, 'coId' : SERVER_ID, 'b2fbe' : 'svr', 'dId' : devId}
     try:
         data = urllib.urlencode(params)
         opener = urllib2.build_opener()
@@ -71,3 +176,4 @@ def getUrlOutcome(url, token, devId):
 
 if __name__ == '__main__':
     main()
+
--- b2factor/b2f.sh
+++ b2factor/b2f.sh
@@ -1,23 +1,25 @@
 #! /bin/bash
 
-if [ -f "/etc/b2factor/b2f.pyc" ]; then
-    python /etc/b2factor/b2f.pyc $B2FID $B2F_DEV_ID
-else
-    python /etc/b2factor/b2f.py $B2FID $B2F_DEV_ID
+#any echo statements that an scp encounters will cause it to fuck up.
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+if [ "$(echo ${SSH_ORIGINAL_COMMAND} | grep '^ssh')" ]; then
+    echo "Checking Blue2Factor ..."
 fi
+python ${__dir}/b2f.pyc $B2FID $B2F_DEV_ID
+
 outcome=$?
-if [ $outcome -eq 0 ]
+if [ $outcome -ne 0 ]
 then
-    if [ "$(echo ${SSH_ORIGINAL_COMMAND} | grep '^scp ')" ]; then
-        ${SSH_ORIGINAL_COMMAND}
-        exit 0
-    else
-        /bin/bash
-    fi
-else
-    echo -e "\n\nThis server is protected by Blue2Factor. Please run your ssh"
-    echo -e "client through the Blue2Factor app and make sure you have "
-    echo -e "another registered device nearby.\n\n\n"
+    echo "This server is protected by Blue2Factor. Please run your ssh client through the Blue2Factor application and make sure you have another registered device nearby."
+    exit 1
 fi
 
+if [ "${SSH_ORIGINAL_COMMAND}" = "" ]; then
+    /bin/bash
+else
+    exec ${SSH_ORIGINAL_COMMAND}
+    if [ "$(echo ${SSH_ORIGINAL_COMMAND} | grep '^ssh')" ]; then
+        echo "Blue2Factor Succeeded"
+    fi
+fi
 exit 0
